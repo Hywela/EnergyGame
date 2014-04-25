6e687f912d8b2d90c638ca7779a1172bb5806f06
Hywela <Kriss_kb@hotmail.com>
2014-04-25 15:50:19 +0200

    mem fiks , freesurface added

--- Source/Texture.cpp
+++ Source/Texture.cpp
@@ -24,7 +24,8 @@ Texture::Texture(const char* imagepath)
 		textureImage->pixels);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
+	SDL_FreeSurface(textureImage);
+	
 }
 
 
